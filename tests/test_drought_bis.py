0e0021e7ff7416302213ff60c611dbedb93c48db
dabakh <el-hadji-abdoul-aziz.kane@grenoble-inp.org>
2024-06-14 11:21:05 +0200

    fix : fixing new format test for the drought component

--- tests/test_drought_bis.py
+++ tests/test_drought_bis.py
@@ -26,48 +26,33 @@ class TestIntegrationDroughtComponent(unittest.TestCase):
         """
         for test_case in self.test_cases:
             with self.subTest(test_case=test_case):
-                print(f"Starting test case: {test_case}")
+
                 precipitation_path = f'../data/tests_data/tests_data_drought/{test_case}_precipitation_test_data.nc'
                 mask_path = f'../data/tests_data/tests_data_drought/{test_case}_mask_test_data.nc'
                 reference_anomalies_path = f'../data/tests_data/tests_data_drought/{test_case}_reference_anomalies.nc'
 
-                # Configurer le composant Drought
-                print("Loading DroughtComponent")
                 drought_component = DroughtComponent(precipitation_path, mask_path)
 
-                # Calculer les anomalies
-                print("Calculating anomalies")
-                calculated_anomalies = drought_component.standardize_max_consecutive_dry_days(self.reference_period, area=False)
+                calculated_anomalies = drought_component.standardize_max_consecutive_dry_days(self.reference_period, area=True)
 
-                # Lire les anomalies de référence
-                print("Loading reference anomalies")
                 reference_anomalies = xr.open_dataset(reference_anomalies_path)
 
-                # Vérification des dimensions
-                print(f"Dimensions in calculated anomalies: {calculated_anomalies.dims}")
-                print(f"Dimensions in reference anomalies: {reference_anomalies.dims}")
 
-                # Comparer les anomalies calculées et de référence
-                print("Calculating mean of anomalies")
-                calculated_mean = calculated_anomalies.mean(dim=['lat', 'lon'])  # Assurez-vous que les dimensions sont correctes
-                reference_mean = reference_anomalies.mean(dim=['lat', 'lon'])  # Assurez-vous que les dimensions sont correctes
+                calculated_df = calculated_anomalies.to_dataframe().reset_index()
+                calculated_df.columns = ['time', 'calculated_mean']
+                reference_df = reference_anomalies.to_dataframe().reset_index()
+                reference_df.columns = ['time', 'reference_mean']
 
-                # Convertir en DataFrame pour comparaison
-                print("Converting to DataFrame")
-                calculated_df = calculated_mean.to_dataframe(name='calculated_mean').reset_index()
-                reference_df = reference_mean.to_dataframe(name='reference_mean').reset_index()
-
-                print("Merging DataFrames")
+        
                 combined_df = pd.merge(calculated_df, reference_df, on='time', suffixes=('_calculated', '_reference')).dropna()
 
-                print("Asserting series equal")
+
                 pd.testing.assert_series_equal(
                     combined_df['calculated_mean'],
                     combined_df['reference_mean'],
                     check_less_precise=True,
                     check_names=False
                 )
-                print(f"Test case {test_case} passed")
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

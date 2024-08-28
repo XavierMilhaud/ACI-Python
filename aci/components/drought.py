fix : fixing last errors during non dask computation

aci/components/drought.py
@@ -71,10 +71,10 @@ class DroughtComponent(Component):
         """
         preci = self.apply_mask("tp") if self.mask is not None else self.array
         precipitation_per_day = preci['tp'].resample(time='d').sum()
-        
+
         # Rechunk data after resampling for optimal performance
         if self.should_use_dask:
-            precipitation_per_day = precipitation_per_day.chunk({'time': -1})        
+            precipitation_per_day = precipitation_per_day.chunk({'time': -1})
         days_below_thresholds = xr.where(precipitation_per_day < 0.001, 1, 0)
         days_above_thresholds = xr.where(days_below_thresholds == 0, 1, 0)
         cumsum_above = days_above_thresholds.cumsum(dim='time')
@@ -166,7 +166,7 @@ class DroughtComponent(Component):
 
         if self.should_use_dask:
             max_days_drought_per_year = max_days_drought_per_year.chunk({'time': -1})
-        
+
         monthly_values = self.drought_interpolate(max_days_drought_per_year)
         # Standardize the interpolated monthly values
         standardized_values = self.standardize_metric(monthly_values, reference_period, area)
@@ -175,4 +175,3 @@ class DroughtComponent(Component):
             standardized_values = standardized_values.compute()
 
         return standardized_values
-    

aci/components/temperature.py
@@ -9,8 +9,10 @@ class TemperatureComponent(Component):
     A class to handle temperature data and perform related calculations.
 
     Attributes:
-        temperature_data (xarray.Dataset): Dataset containing temperature data.
-        mask_data (xarray.Dataset or None): Dataset containing mask data, if provided.
+        temperature_data (xarray.Dataset): Dataset containing temperature
+        data.
+        mask_data (xarray.Dataset or None): Dataset containing mask data,
+        if provided.
     """
 
     def __init__(self, temperature_data_source, mask_data_path=None):
@@ -20,7 +22,8 @@ class TemperatureComponent(Component):
         Parameters:
         ----------
         temperature_data_source : str
-            Path to a directory containing NetCDF files or a single NetCDF file for temperature data.
+            Path to a directory containing NetCDF files or a single NetCDF
+            file for temperature data.
         mask_data_path : str, optional
             Path to the dataset containing mask data. Default is None.
 
@@ -33,7 +36,8 @@ class TemperatureComponent(Component):
         if os.path.isdir(temperature_data_source):
             # Load multiple NetCDF files using open_mfdataset
             temperature_data = xr.open_mfdataset(
-                os.path.join(temperature_data_source, "*.nc"), combine='by_coords'
+                os.path.join(
+                    temperature_data_source, "*.nc"), combine='by_coords'
             )
         else:
             # Load a single NetCDF file
@@ -42,7 +46,8 @@ class TemperatureComponent(Component):
         # Load mask data if provided
         mask_data = None
         if mask_data_path:
-            mask_data = xr.open_dataset(mask_data_path).rename({'lon': 'longitude', 'lat': 'latitude'})
+            mask_data = xr.open_dataset(
+                mask_data_path).rename({'lon': 'longitude', 'lat': 'latitude'})
 
         super().__init__(temperature_data, mask_data, temperature_data_source)
 
@@ -92,7 +97,8 @@ class TemperatureComponent(Component):
 
     def percentiles(self, n, reference_period, tempo):
         """
-        Compute percentiles for day or night temperatures over a reference period.
+        Compute percentiles for day or night temperatures over a reference
+        period.
 
         Parameters:
         ----------
@@ -127,14 +133,17 @@ class TemperatureComponent(Component):
 
         if self.should_use_dask:
             # Use Dask for parallel computation
-            temperature_reference = temperature_reference.chunk({'time': -1})
+            temperature_reference = temperature_reference.chunk(
+                {'time': -1})
 
             def compute_percentile(arr, q):
                 return np.percentile(arr, q, axis=-1)
-            
-            rolling = temperature_reference["t2m"].rolling(time=rolling_window_size, min_periods=1, center=True)
+
+            rolling = temperature_reference["t2m"].rolling(
+                time=rolling_window_size, min_periods=1, center=True)
             rolling_constructed = rolling.construct('window_dim')
-            rolling_constructed = rolling_constructed.chunk({'time': -1})
+            rolling_constructed = rolling_constructed.chunk(
+                {'time': -1})
 
             percentile_reference = xr.apply_ufunc(
                 compute_percentile,
@@ -156,7 +165,8 @@ class TemperatureComponent(Component):
             )
         else:
             # Non-Dask computation
-            rolling = temperature_reference["t2m"].rolling(time=rolling_window_size, min_periods=1, center=True)
+            rolling = temperature_reference["t2m"].rolling(
+                time=rolling_window_size, min_periods=1, center=True)
             rolling_constructed = rolling.construct('window_dim')
             percentile_reference = rolling_constructed.reduce(
                 lambda arr, axis: np.percentile(arr, n, axis=axis),
@@ -312,7 +322,8 @@ class TemperatureComponent(Component):
         O(N), where N is the number of time steps in the study period.
         """
         t90_values = self.t90(reference_period)
-        std_t90_values = self.standardize_metric(t90_values, reference_period, area)
+        std_t90_values = self.standardize_metric(
+            t90_values, reference_period, area)
 
         if not self.should_use_dask:
             std_t90_values = std_t90_values.compute()
@@ -328,7 +339,8 @@ class TemperatureComponent(Component):
         reference_period : tuple
             Start and end dates of the reference period.
         area : bool, optional
-            If True, compute the area-averaged standardized metric. Default is None.
+            If True, compute the area-averaged standardized metric.
+            Default is None.
 
         Returns:
         -------
@@ -340,7 +352,8 @@ class TemperatureComponent(Component):
         O(N), where N is the number of time steps in the study period.
         """
         t10_values = self.t10(reference_period)
-        std_t10_values = self.standardize_metric(t10_values, reference_period, area)
+        std_t10_values = self.standardize_metric(
+            t10_values, reference_period, area)
 
         if not self.should_use_dask:
             std_t10_values = std_t10_values.compute()

tests/test_drought.py
@@ -3,6 +3,7 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import os
+import tempfile
 import sys
 import warnings
 
@@ -11,18 +12,19 @@ from drought import DroughtComponent
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-
 class TestDrought(unittest.TestCase):
 
     def setUp(self):
         """
-        Setup test data.
+        Setup test data in a temporary directory.
         """
-        self.mask_path = "test_mask.nc"
-        self.data_path = 'test_data.nc'
+        # Créer un répertoire temporaire pour les fichiers de test
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.data_path = os.path.join(self.temp_dir.name, 'test_data.nc')
+        self.mask_path = os.path.join(self.temp_dir.name, 'test_mask.nc')
         self.reference_period = ('2000-01-01', '2009-12-31')
 
-        # Generating test precipitation data
+        # Génération des données de précipitation pour les tests
         times = pd.date_range('2000-01-01', '2020-12-31', freq='D')
         latitudes = np.arange(48.80, 48.90, 0.1)
         longitudes = np.arange(2.20, 2.30, 0.1)
@@ -35,7 +37,7 @@ class TestDrought(unittest.TestCase):
         )
         data.to_netcdf(self.data_path)
 
-        # Generating test mask data
+        # Génération des données de masque pour les tests
         mask_data = np.ones((len(latitudes), len(longitudes)))
         mask = xr.Dataset(
             {'country': (['lat', 'lon'], mask_data)},
@@ -43,8 +45,7 @@ class TestDrought(unittest.TestCase):
         )
         mask.to_netcdf(self.mask_path)
 
-        # Initializing precomputed testing data parameters
-
+        # Initialisation des paramètres pour les données de test pré-calculées
         self.test_cases = ['test1', 'test2', 'test3', 'test4']
         self.reference_period_bis = ('2000-01-01', '2000-12-31')
         self.study_period_bis = ('2000-01-01', '2001-12-31')
@@ -53,8 +54,8 @@ class TestDrought(unittest.TestCase):
         """
         Clean up test data files.
         """
-        os.remove(self.data_path)
-        os.remove(self.mask_path)
+        # Supprimer le répertoire temporaire et tous les fichiers qu'il contient
+        self.temp_dir.cleanup()
 
     def test_std_max_consecutive_dry_days(self):
         """
@@ -78,10 +79,6 @@ class TestDrought(unittest.TestCase):
         # Ensure that the mean anomaly over the reference period is approximately zero
         ref_anomalies = anomalies.sel(time=slice(self.reference_period[0], self.reference_period[1]))
 
-        # Compute if using Dask to avoid NotImplementedError
-        if drought.use_dask:
-            ref_anomalies = ref_anomalies.compute()
-
         mean_anomaly = ref_anomalies.mean().item()
         self.assertFalse(np.isnan(mean_anomaly), "Mean anomaly should not be NaN.")
         self.assertAlmostEqual(mean_anomaly, 0, places=1)
@@ -90,7 +87,6 @@ class TestDrought(unittest.TestCase):
         std_anomaly = ref_anomalies.std().item()
         self.assertFalse(np.isnan(std_anomaly), "Std anomaly should not be NaN.")
         self.assertAlmostEqual(std_anomaly, 1, places=1)
-
 
     def test_no_precipitation(self):
         """
@@ -110,7 +106,6 @@ class TestDrought(unittest.TestCase):
         data.to_netcdf(self.data_path)
 
         drought = DroughtComponent(self.data_path, self.mask_path)
-
         anomalies = drought.std_max_consecutive_dry_days(self.reference_period)
 
         self.assertTrue(np.all(np.isnan(anomalies)), "Anomalies should be NaN when there is no precipitation.")
@@ -133,16 +128,11 @@ class TestDrought(unittest.TestCase):
         data.to_netcdf(self.data_path)
 
         drought = DroughtComponent(self.data_path, self.mask_path)
-
         anomalies = drought.std_max_consecutive_dry_days(self.reference_period)
         cal = drought.max_consecutive_dry_days()
 
-        self.assertTrue(np.all(np.isnan(anomalies)),
-                        "Anomalies should be NaN when precipitation is constant below the threshold"
-                    )
-        self.assertTrue(np.all(cal == cal[0, 0, 0]),
-                        "Max consecutive dry days should be the same when precipitation is constant and below the threshold."
-                    )
+        self.assertTrue(np.all(np.isnan(anomalies)), "Anomalies should be NaN when precipitation is constant below the threshold.")
+        self.assertTrue(np.all(cal == cal[0, 0, 0]), "Max consecutive dry days should be the same when precipitation is constant and below the threshold.")
 
     def test_standardize_drought(self):
         """
@@ -179,7 +169,6 @@ class TestDrought(unittest.TestCase):
                     check_exact=False,
                     check_names=False
                 )
-
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)

tests/test_precipitation.py
@@ -3,49 +3,39 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import os
+import tempfile
 import sys
-from dask.distributed import Client, LocalCluster
-sys.path.insert(
-    0, os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__), '../aci/components')))
 import warnings
+
+# Ajouter le chemin du module PrecipitationComponent
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../aci/components')))
 from precipitation import PrecipitationComponent
 
-warnings.filterwarnings(
-    "ignore", category=DeprecationWarning)
+# Ignorer les warnings de dépréciation
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 class TestPrecipitation(unittest.TestCase):
 
-    @classmethod
-    def setUpClass(cls):
-        cls.cluster = LocalCluster(n_workers=4, threads_per_worker=1)
-        cls.client = Client(cls.cluster)
-
-    @classmethod
-    def tearDownClass(cls):
-        cls.client.close()
-        cls.cluster.close()
-
     def setUp(self):
         """
-        Setup test data.
+        Setup test data in a temporary directory.
         """
-        self.mask_path = "test_mask.nc"
+        # Créer un répertoire temporaire pour les fichiers de test
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.data_path = os.path.join(self.temp_dir.name, 'test_data.nc')
+        self.mask_path = os.path.join(self.temp_dir.name, 'test_mask.nc')
+
+        # Génération des données de précipitation pour les tests
         times = pd.date_range('2000-01-01', '2020-12-31', freq='D')
         latitudes = np.arange(48.80, 48.90, 0.1)
         longitudes = np.arange(2.20, 2.30, 0.1)
 
         np.random.seed(0)
-        precipitation_data = np.random.rand(
-            len(times), len(latitudes), len(longitudes))
-        self.data_path = 'test_data.nc'
+        precipitation_data = np.random.rand(len(times), len(latitudes), len(longitudes))
 
-        tp_data = {
-            'tp': (['time', 'latitude', 'longitude'], precipitation_data)}
-        coords = {
-            'time': times, 'latitude': latitudes, 'longitude': longitudes}
+        tp_data = {'tp': (['time', 'latitude', 'longitude'], precipitation_data)}
+        coords = {'time': times, 'latitude': latitudes, 'longitude': longitudes}
 
         data = xr.Dataset(tp_data, coords=coords)
         data.to_netcdf(self.data_path)
@@ -57,9 +47,8 @@ class TestPrecipitation(unittest.TestCase):
             coords={'lat': latitudes, 'lon': longitudes}
         )
         mask.to_netcdf(self.mask_path)
-        self.reference_period = ('2000-01-01', '2009-12-31')
 
-        # Ajout des nouveaux tests
+        self.reference_period = ('2000-01-01', '2009-12-31')
         self.reference_period_bis = ('2000-01-01', '2000-12-31')
         self.data_dir = '../data/tests_data/tests_data_prec_bis'
 
@@ -67,8 +56,8 @@ class TestPrecipitation(unittest.TestCase):
         """
         Clean up test data files.
         """
-        os.remove(self.data_path)
-        os.remove(self.mask_path)
+        # Supprimer le répertoire temporaire et tous les fichiers qu'il contient
+        self.temp_dir.cleanup()
 
     def test_monthly_max_anomaly(self):
         """
@@ -97,19 +86,15 @@ class TestPrecipitation(unittest.TestCase):
         dt = slice(self.reference_period[0], self.reference_period[1])
         ref_anomalies = anomalies.sel(time=dt)
 
-        # Calculate the mean anomaly with compute to finalize the Dask computation
-        mean_anomaly = ref_anomalies.mean().compute()
-        mean_anomaly_value = mean_anomaly.item() if hasattr(mean_anomaly, 'item') else mean_anomaly
-
-        self.assertFalse(np.isnan(mean_anomaly_value), "Mean anomaly should not be NaN.")
-        self.assertAlmostEqual(mean_anomaly_value, 0, places=1)
+        # Calculate the mean anomaly
+        mean_anomaly = ref_anomalies.mean().item()
+        self.assertFalse(np.isnan(mean_anomaly), "Mean anomaly should not be NaN.")
+        self.assertAlmostEqual(mean_anomaly, 0, places=1)
 
         # Ensure that the standard deviation of anomalies over the reference period is approximately one
-        std_anomaly = ref_anomalies.std().compute()
-        std_anomaly_value = std_anomaly.item() if hasattr(std_anomaly, 'item') else std_anomaly
-
-        self.assertFalse(np.isnan(std_anomaly_value), "Std anomaly should not be NaN.")
-        self.assertAlmostEqual(std_anomaly_value, 1, places=1)
+        std_anomaly = ref_anomalies.std().item()
+        self.assertFalse(np.isnan(std_anomaly), "Std anomaly should not be NaN.")
+        self.assertAlmostEqual(std_anomaly, 1, places=1)
 
     def test_no_precipitation(self):
         """
@@ -120,26 +105,18 @@ class TestPrecipitation(unittest.TestCase):
         longitudes = np.arange(2.20, 2.30, 0.1)
 
         # All zeros for no precipitation
-        precipitation_data = np.zeros(
-            (len(times), len(latitudes), len(longitudes)))
+        precipitation_data = np.zeros((len(times), len(latitudes), len(longitudes)))
 
-        tp_data = {
-            'tp': (['time', 'latitude', 'longitude'], precipitation_data)}
-        coords = {
-            'time': times, 'latitude': latitudes, 'longitude': longitudes}
+        tp_data = {'tp': (['time', 'latitude', 'longitude'], precipitation_data)}
+        coords = {'time': times, 'latitude': latitudes, 'longitude': longitudes}
 
         data = xr.Dataset(tp_data, coords=coords)
         data.to_netcdf(self.data_path)
 
-        precipitation = PrecipitationComponent(
-            self.data_path, self.mask_path)
+        precipitation = PrecipitationComponent(self.data_path, self.mask_path)
+        anomalies = precipitation.monthly_max_anomaly('tp', 5, self.reference_period)
 
-        anomalies = precipitation.monthly_max_anomaly(
-            'tp', 5, self.reference_period)
-
-        self.assertTrue(
-            np.all(np.isnan(anomalies)),
-            "Anomalies should be NaN when there is no precipitation.")
+        self.assertTrue(np.all(np.isnan(anomalies)), "Anomalies should be NaN when there is no precipitation.")
 
     def test_constant_precipitation(self):
         """
@@ -150,55 +127,42 @@ class TestPrecipitation(unittest.TestCase):
         longitudes = np.arange(2.20, 2.30, 0.1)
 
         # Constant precipitation value
-        precipitation_data = np.full(
-            (len(times), len(latitudes), len(longitudes)), 10)
+        precipitation_data = np.full((len(times), len(latitudes), len(longitudes)), 10)
 
-        tp_data = {
-            'tp': (['time', 'latitude', 'longitude'], precipitation_data)}
-        coords = {
-            'time': times, 'latitude': latitudes, 'longitude': longitudes}
+        tp_data = {'tp': (['time', 'latitude', 'longitude'], precipitation_data)}
+        coords = {'time': times, 'latitude': latitudes, 'longitude': longitudes}
 
         data = xr.Dataset(tp_data, coords=coords)
         data.to_netcdf(self.data_path)
 
-        precipitation = PrecipitationComponent(
-            self.data_path, self.mask_path)
+        precipitation = PrecipitationComponent(self.data_path, self.mask_path)
+        anomalies = precipitation.monthly_max_anomaly('tp', 5, self.reference_period)
 
-        anomalies = precipitation.monthly_max_anomaly(
-            'tp', 5, self.reference_period)
-
-        self.assertTrue(
-            np.all(
-                np.isnan(anomalies)),
-            "Anomalies should be NaN when precipitation is constant.")
+        self.assertTrue(np.all(np.isnan(anomalies)), "Anomalies should be NaN when precipitation is constant.")
 
     def test_monthly_max_anomaly_bis(self):
+        """
+        Test the monthly_max_anomaly method with different test cases.
+        """
         test_cases = ['test1', 'test2', 'test3', 'test4']
 
         for test_case in test_cases:
             with self.subTest(test_case=test_case):
-                data_path = os.path.join(
-                    self.data_dir, f'{test_case}_data.nc')
-                mask_path = os.path.join(
-                    self.data_dir, f'{test_case}_mask.nc')
-                reference_anomalies_path = os.path.join(
-                    self.data_dir, f'{test_case}_reference_anomalies.nc')
+                data_path = os.path.join(self.data_dir, f'{test_case}_data.nc')
+                mask_path = os.path.join(self.data_dir, f'{test_case}_mask.nc')
+                reference_anomalies_path = os.path.join(self.data_dir, f'{test_case}_reference_anomalies.nc')
 
                 # Lire les anomalies de référence
-                reference_anomalies = xr.open_dataset(
-                    reference_anomalies_path)
+                reference_anomalies = xr.open_dataset(reference_anomalies_path)
 
                 # Initialiser la composante précipitation
-                precipitation = PrecipitationComponent(
-                    data_path, mask_path)
+                precipitation = PrecipitationComponent(data_path, mask_path)
 
                 # Calculer les anomalies
-                anomalies = precipitation.monthly_max_anomaly(
-                    'tp', 5, self.reference_period_bis)
+                anomalies = precipitation.monthly_max_anomaly('tp', 5, self.reference_period_bis)
 
                 # Comparer avec les anomalies de référence
-                np.testing.assert_allclose(
-                    anomalies.values, reference_anomalies['tp'].values)
+                np.testing.assert_allclose(anomalies.values, reference_anomalies['tp'].values)
 
 
 if __name__ == '__main__':

tests/test_wind.py
@@ -3,14 +3,11 @@ import xarray as xr
 import numpy as np
 import pandas as pd
 import os
+import tempfile
 import sys
 import warnings
 
-sys.path.insert(
-    0,
-    os.path.abspath(
-        os.path.join(
-            os.path.dirname(__file__), '../aci/components')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../aci/components')))
 from wind import WindComponent
 
 warnings.filterwarnings("ignore", category=DeprecationWarning)
@@ -21,34 +18,30 @@ class TestWindComponent(unittest.TestCase):
 
     def setUp(self):
         """
-        Setup test data.
+        Setup test data in a temporary directory.
         """
-        self.mask_path = "test_mask.nc"
+        # Créer un répertoire temporaire pour les fichiers de test
+        self.temp_dir = tempfile.TemporaryDirectory()
+        self.u10_path = os.path.join(self.temp_dir.name, 'test_u10.nc')
+        self.v10_path = os.path.join(self.temp_dir.name, 'test_v10.nc')
+        self.mask_path = os.path.join(self.temp_dir.name, 'test_mask.nc')
+        self.reference_period = ('2000-01-01', '2009-12-31')
+
+        # Génération des données de vent pour les tests
         times = pd.date_range('2000-01-01', '2020-12-31', freq='D')
         latitudes = np.arange(48.80, 48.90, 0.1)
         longitudes = np.arange(2.20, 2.30, 0.1)
 
         np.random.seed(0)
-        wind_data = np.random.rand(
-            len(times),
-            len(latitudes),
-            len(longitudes))
-        self.u10_path = 'test_u10.nc'
-        self.v10_path = 'test_v10.nc'
+        wind_data = np.random.rand(len(times), len(latitudes), len(longitudes))
 
         u10_data = xr.Dataset(
             {'u10': (['time', 'latitude', 'longitude'], wind_data)},
-            coords={
-                'time': times,
-                'latitude': latitudes,
-                'longitude': longitudes}
+            coords={'time': times, 'latitude': latitudes, 'longitude': longitudes}
         )
         v10_data = xr.Dataset(
             {'v10': (['time', 'latitude', 'longitude'], wind_data)},
-            coords={
-                'time': times,
-                'latitude': latitudes,
-                'longitude': longitudes}
+            coords={'time': times, 'latitude': latitudes, 'longitude': longitudes}
         )
         u10_data.to_netcdf(self.u10_path)
         v10_data.to_netcdf(self.v10_path)
@@ -61,10 +54,7 @@ class TestWindComponent(unittest.TestCase):
         )
         mask.to_netcdf(self.mask_path)
 
-        self.reference_period = ('2000-01-01', '2009-12-31')
-
-        # Setting up testing parameters for last method
-
+        # Initialisation des paramètres pour les données de test
         self.reference_period_bis = ('2000-01-01', '2000-12-31')
         self.data_dir = '../data/tests_data/tests_data_wind'
 
@@ -72,18 +62,14 @@ class TestWindComponent(unittest.TestCase):
         """
         Clean up test data files.
         """
-        os.remove(self.u10_path)
-        os.remove(self.v10_path)
-        os.remove(self.mask_path)
+        # Supprimer le répertoire temporaire et tous les fichiers qu'il contient
+        self.temp_dir.cleanup()
 
     def test_wind_power(self):
         """
         Test the wind_power method.
         """
-        wind = WindComponent(
-            self.u10_path,
-            self.v10_path,
-            self.mask_path)
+        wind = WindComponent(self.u10_path, self.v10_path, self.mask_path)
         wind_power = wind.wind_power()
 
         # Verify that wind_power is a DataArray
@@ -95,28 +81,18 @@ class TestWindComponent(unittest.TestCase):
         self.assertIn('longitude', wind_power.dims)
 
         # Check that the result contains the correct time period
-        self.assertGreaterEqual(
-            wind_power['time'].min(),
-            np.datetime64('2000-01-01'))
-        self.assertLessEqual(
-            wind_power['time'].max(),
-            np.datetime64('2020-12-31'))
+        self.assertGreaterEqual(wind_power['time'].min(), np.datetime64('2000-01-01'))
+        self.assertLessEqual(wind_power['time'].max(), np.datetime64('2020-12-31'))
 
     def test_wind_thresholds(self):
         """
         Test the wind_thresholds method.
         """
-        wind = WindComponent(
-            self.u10_path,
-            self.v10_path,
-            self.mask_path)
-        wind_thresholds = wind.wind_thresholds(
-            self.reference_period)
+        wind = WindComponent(self.u10_path, self.v10_path, self.mask_path)
+        wind_thresholds = wind.wind_thresholds(self.reference_period)
 
         # Verify that wind_thresholds is a DataArray
-        self.assertIsInstance(
-            wind_thresholds,
-            xr.DataArray)
+        self.assertIsInstance(wind_thresholds, xr.DataArray)
 
         # Check the dimensions of the result
         self.assertIn('time', wind_thresholds.dims)
@@ -124,28 +100,18 @@ class TestWindComponent(unittest.TestCase):
         self.assertIn('longitude', wind_thresholds.dims)
 
         # Check that the result contains the correct time period
-        self.assertGreaterEqual(
-            wind_thresholds['time'].min(),
-            np.datetime64('2000-01-01'))
-        self.assertLessEqual(
-            wind_thresholds['time'].max(),
-            np.datetime64('2020-12-31'))
+        self.assertGreaterEqual(wind_thresholds['time'].min(), np.datetime64('2000-01-01'))
+        self.assertLessEqual(wind_thresholds['time'].max(), np.datetime64('2020-12-31'))
 
     def test_days_above_thresholds(self):
         """
         Test the days_above_thresholds method.
         """
-        wind = WindComponent(
-            self.u10_path,
-            self.v10_path,
-            self.mask_path)
-        days_above_thresholds = wind.days_above_thresholds(
-            self.reference_period)
+        wind = WindComponent(self.u10_path, self.v10_path, self.mask_path)
+        days_above_thresholds = wind.days_above_thresholds(self.reference_period)
 
         # Verify that days_above_thresholds is a DataArray
-        self.assertIsInstance(
-            days_above_thresholds,
-            xr.DataArray)
+        self.assertIsInstance(days_above_thresholds, xr.DataArray)
 
         # Check the dimensions of the result
         self.assertIn('time', days_above_thresholds.dims)
@@ -153,28 +119,18 @@ class TestWindComponent(unittest.TestCase):
         self.assertIn('longitude', days_above_thresholds.dims)
 
         # Check that the result contains the correct time period
-        self.assertGreaterEqual(
-            days_above_thresholds['time'].min(),
-            np.datetime64('2000-01-01'))
-        self.assertLessEqual(
-            days_above_thresholds['time'].max(),
-            np.datetime64('2020-12-31'))
+        self.assertGreaterEqual(days_above_thresholds['time'].min(), np.datetime64('2000-01-01'))
+        self.assertLessEqual(days_above_thresholds['time'].max(), np.datetime64('2020-12-31'))
 
     def test_wind_exceedance_frequency(self):
         """
         Test the wind_exceedance_frequency method.
         """
-        wind = WindComponent(
-            self.u10_path,
-            self.v10_path,
-            self.mask_path)
-        wind_exceedance_frequency = wind.wind_exceedance_frequency(
-            self.reference_period)
+        wind = WindComponent(self.u10_path, self.v10_path, self.mask_path)
+        wind_exceedance_frequency = wind.wind_exceedance_frequency(self.reference_period)
 
         # Verify that wind_exceedance_frequency is a DataArray
-        self.assertIsInstance(
-            wind_exceedance_frequency,
-            xr.DataArray)
+        self.assertIsInstance(wind_exceedance_frequency, xr.DataArray)
 
         # Check the dimensions of the result
         self.assertIn('time', wind_exceedance_frequency.dims)
@@ -182,28 +138,18 @@ class TestWindComponent(unittest.TestCase):
         self.assertIn('longitude', wind_exceedance_frequency.dims)
 
         # Check that the result contains the correct time period
-        self.assertGreaterEqual(
-            wind_exceedance_frequency['time'].min(),
-            np.datetime64('2000-01-01'))
-        self.assertLessEqual(
-            wind_exceedance_frequency['time'].max(),
-            np.datetime64('2020-12-31'))
+        self.assertGreaterEqual(wind_exceedance_frequency['time'].min(), np.datetime64('2000-01-01'))
+        self.assertLessEqual(wind_exceedance_frequency['time'].max(), np.datetime64('2020-12-31'))
 
     def test_std_wind_exceedance_frequency(self):
         """
         Test the std_wind_exceedance_frequency method.
         """
-        wind = WindComponent(
-            self.u10_path,
-            self.v10_path,
-            self.mask_path)
-        standardized_frequency = wind.std_wind_exceedance_frequency(
-            self.reference_period)
+        wind = WindComponent(self.u10_path, self.v10_path, self.mask_path)
+        standardized_frequency = wind.std_wind_exceedance_frequency(self.reference_period)
 
         # Verify that standardized_frequency is a DataArray
-        self.assertIsInstance(
-            standardized_frequency,
-            xr.DataArray)
+        self.assertIsInstance(standardized_frequency, xr.DataArray)
 
         # Check the dimensions of the result
         self.assertIn('time', standardized_frequency.dims)
@@ -211,59 +157,42 @@ class TestWindComponent(unittest.TestCase):
         self.assertIn('longitude', standardized_frequency.dims)
 
         # Check that the result contains the correct time period
-        self.assertGreaterEqual(
-            standardized_frequency['time'].min(),
-            np.datetime64('2000-01-01'))
-        self.assertLessEqual(
-            standardized_frequency['time'].max(),
-            np.datetime64('2020-12-31'))
+        self.assertGreaterEqual(standardized_frequency['time'].min(), np.datetime64('2000-01-01'))
+        self.assertLessEqual(standardized_frequency['time'].max(), np.datetime64('2020-12-31'))
 
         # Ensure that the mean standardized frequency over the reference period is approximately zero
         dt = slice(self.reference_period[0], self.reference_period[1])
         ref_standardized_frequency = standardized_frequency.sel(time=dt)
 
-        # Compute if using Dask to avoid NotImplementedError
-        if wind.use_dask:
-            ref_standardized_frequency = ref_standardized_frequency.compute()
-
         mean_standardized_frequency = ref_standardized_frequency.mean().item()
-        self.assertFalse(
-            np.isnan(mean_standardized_frequency),
-            "Mean standardized frequency should not be NaN.")
+        self.assertFalse(np.isnan(mean_standardized_frequency), "Mean standardized frequency should not be NaN.")
         self.assertAlmostEqual(mean_standardized_frequency, 0, places=1)
 
         # Ensure that the standard deviation of the standardized frequency over the reference period is approximately one
         std_standardized_frequency = ref_standardized_frequency.std().item()
-        self.assertFalse(
-            np.isnan(std_standardized_frequency),
-            "Std standardized frequency should not be NaN.")
+        self.assertFalse(np.isnan(std_standardized_frequency), "Std standardized frequency should not be NaN.")
         self.assertAlmostEqual(std_standardized_frequency, 1, places=1)
 
     def test_std_wind_exceedance_frequency_bis(self):
+        """
+        Test the std_wind_exceedance_frequency method with different test cases.
+        """
         test_cases = ['test1', 'test2', 'test3', 'test4']
 
         for test_case in test_cases:
             with self.subTest(test_case=test_case):
-
                 u10_path = os.path.join(self.data_dir, f'{test_case}_u10.nc')
                 v10_path = os.path.join(self.data_dir, f'{test_case}_v10.nc')
                 mask_path = os.path.join(self.data_dir, f'{test_case}_mask.nc')
-                reference_anomalies_path = os.path.join(
-                    self.data_dir,
-                    f'{test_case}_reference_anomalies.nc')
+                reference_anomalies_path = os.path.join(self.data_dir, f'{test_case}_reference_anomalies.nc')
 
                 reference_anomalies = xr.open_dataset(reference_anomalies_path)
                 w_com = WindComponent(u10_path, v10_path, mask_path)
 
-                calculated_anomalies = w_com.std_wind_exceedance_frequency(
-                    self.reference_period_bis,
-                    area=True)
+                calculated_anomalies = w_com.std_wind_exceedance_frequency(self.reference_period_bis, area=True)
 
-                reference_variable_name = list(
-                    reference_anomalies.data_vars)[0]
-                np.testing.assert_allclose(
-                    calculated_anomalies.values,
-                    reference_anomalies[reference_variable_name].values)
+                reference_variable_name = list(reference_anomalies.data_vars)[0]
+                np.testing.assert_allclose(calculated_anomalies.values, reference_anomalies[reference_variable_name].values)
 
 
 if __name__ == '__main__':